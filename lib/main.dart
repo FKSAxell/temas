0cd6b7ca9b36ecf44381f00a9e8b11c1dd7beb59
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2021-04-05 15:18:59 -0500

    themeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,3 @@
-// import 'package:temas/src/pages/animaciones_page.dart';
-// import 'package:temas/src/labs/circular_progressbar_page.dart';
-// import 'package:temas/src/pages/emergency_page.dart';
-// import 'package:temas/src/pages/graficas_circulares_page.dart';
-// import 'package:temas/src/pages/headers_page.dart';
-// import 'package:temas/src/labs/slideshow_page.dart';
-// import 'package:temas/src/pages/pinterest_page.dart';
-// import 'package:temas/src/pages/sliver_list_page.dart';
-// import 'package:temas/src/retos/cuadrado_animado_page.dart';
-// import 'src/pages/slideshow_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:temas/src/pages/launcher_page.dart';
@@ -15,7 +5,7 @@ import 'package:temas/src/theme/theme.dart';
 
 void main() => runApp(
       ChangeNotifierProvider(
-        create: (_) => ThemeChanger(),
+        create: (_) => ThemeChanger(2),
         child: MyApp(),
       ),
     );
@@ -23,10 +13,12 @@ void main() => runApp(
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final currentTheme = Provider.of<ThemeChanger>(context).currentTheme;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños App',
       home: LauncherPage(),
+      theme: currentTheme,
     );
   }
 }
